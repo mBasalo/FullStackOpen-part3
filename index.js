86436420e36a2d8b5fd14935d02e532ea84c1565
Mauricio Basalo <mbasalogutierrez@gmail.com>
2024-11-19 11:21:49 -0300

    3.3

--- index.js
+++ index.js
@@ -25,6 +25,17 @@ let persons = [
     }
 ]
 
+app.get('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    const person = persons.find(person => person.id === id)
+    
+    if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+  })
+
 
 app.get('/api/persons', (request, response) => {
     response.json(persons)
