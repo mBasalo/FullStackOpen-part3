27ad63647efce133c3fd516995c0d6f7f88c6ea5
Mauricio Basalo <mbasalogutierrez@gmail.com>
2024-11-19 12:06:05 -0300

    3.5

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
+
 
 let persons = [
     { 
@@ -24,6 +27,47 @@ let persons = [
       "number": "39-23-6423122"
     }
 ]
+
+
+// GENERATE ID
+
+const generateId = () => {
+  const maxId = persons.length > 0
+    ? Math.max(...persons.map(p => Number(p.id)))
+    : 0;
+  return String(maxId + 1);
+}
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body;
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: 'name or number missing'
+    });
+  }
+
+
+  if (persons.some(person => person.name === body.name)) {
+    return response.status(400).json({
+      error: 'name must be unique'
+    });
+  }
+
+
+  const person = {
+    id: generateId(),
+    name: body.name,
+    number: body.number,
+  };
+
+  persons = persons.concat(person);
+
+  response.json(person);
+});
+
+
+// ----------------------------------------------------------------
 
 app.get('/api/persons/:id', (request, response) => {
     const id = request.params.id
